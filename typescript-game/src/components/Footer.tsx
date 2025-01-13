2d0df4b69bda48ee0ba6c800f1d52e1f64297ba5
dev2024dev <publicdev2024@gmail.com>
2025-01-14 01:12:19 +0900

    Create basic structure for header and footer

--- typescript-game/src/components/Footer.tsx
+++ typescript-game/src/components/Footer.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Footer = () => {
-  const footerStyle = { width: '100%', height: '10vh', backgroundColor: 'blue' };
-  return <div style={footerStyle}>Footer</div>;
+  const footerStyle = { width: '100%', height: '10vh', backgroundColor: 'orange', display: 'flex', alignItems: 'center'};
+  const liStyle = { width: '100%', height: '100%', listStyle: 'none', padding: 0, margin: 0, display: 'flex', justifyContent: 'space-around', alignItems: 'center'};
+  const ulStyle = { padding: 0, margin: 0, color: 'white'}
+  return <div style={footerStyle}>
+    <li style={liStyle}>
+      <ul style={ulStyle}>
+        Contact: publicdev2024@gmail.com
+      </ul>
+      <ul style={ulStyle}>
+        Designed and developed by JaeHyung
+      </ul>
+    </li>
+  </div>;
 };
 
 export default Footer;
--- typescript-game/src/components/Header.tsx
+++ typescript-game/src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import diceLogo from '../assets/imgs/DiceGameLogo.png';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 
 const Header = () => {
   const headerStyle = { width: '100%', height: '10vh', display: 'flex', alignItems: 'center', justifyContent: 'space-between', backgroundColor: 'green' };
-  const logoStyle = { height: '100%', aspectRatio: 1 };
+  const logoStyle = { height: '100%', aspectRatio: 1, marginLeft: '1rem' };
   const mainNameStyle = { fontSize: '3rem', fontWeight: 'bold', color: 'white' };
-  const toggleBtnStyle = { height: '70%', aspectRatio: 1 };
+  const toggleBtnStyle = { height: '70%', aspectRatio: 1 ,marginRight: '1rem', background: 'none', border: 'none'};
+  const whiteColor = { color: 'white'};
 
   return (
     <div style={headerStyle}>
@@ -15,7 +17,7 @@ const Header = () => {
         style={toggleBtnStyle}
         onClick={() => alert('Button clicked!')}
       >
-        Toggle
+        <DensityMediumIcon style={whiteColor}/>
       </button>
     </div>
   );
